1ed0f91d63b38d3823cd4b8e991cf0c6c632e769
alan vergara <alan_vergara_p@hotmail.com>
2022-06-20 17:34:29 -0500

    created basic app before implement Web Socket

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(messages_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
+  end
+
+  private
+
+  def messages_params
+    params.require(:message).permit(:content)
+  end
+
+end
